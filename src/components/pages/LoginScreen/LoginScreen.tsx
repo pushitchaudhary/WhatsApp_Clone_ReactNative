a8722f9fe744e446e57495c517ce75d0eef67444
Pushit Chaudhary <56717453+PushitChaudhary@users.noreply.github.com>
2024-08-09 17:53:06 +0545

    added some desgin

--- src/components/pages/LoginScreen/LoginScreen.tsx
+++ src/components/pages/LoginScreen/LoginScreen.tsx
@@ -5,7 +5,15 @@ import React from 'react'
 const LoginScreen = () => {
   return (
     <SafeAreaView>
-      <Text>LoginScreen</Text>
+        <View style={styles.root}>
+            <View>
+            <Text style={styles.title}>Enter your phone number</Text>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>WhatsApp will need to verify your phone number. What's my number?</Text>
+            </View>
+
+        </View>
     </SafeAreaView>
   
   )
@@ -13,4 +21,24 @@ const LoginScreen = () => {
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    root : {
+        display : 'flex',
+        height : '100%',
+        borderWidth : 2,
+        borderColor : 'red',
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    title : {
+        fontSize : 23,
+        fontWeight : '600'
+    },
+    textContainer : {
+        borderWidth : 2,
+    },
+    text : {
+        fontSize : 13,
+        paddingHorizontal : 12
+    }
+})
